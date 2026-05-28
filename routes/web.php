c0ca11ff665bbb24c29ba67a9de925c071179f11
CatBounceXD <steventrue737@gmail.com>
2026-05-28 23:03:10 +0700

    feat: menambahkan Master Layout, fitur RPS

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BahanAjarController;
+use App\Http\Controllers\RpsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/bahan-ajar', [BahanAjarController::class, 'index']);
+Route::get('/rps', [RpsController::class, 'index']);
