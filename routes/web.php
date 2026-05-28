9e58a99d4e6bdd7c40be3c6f08b9f8353004241d
unknown <syafiqaaida9@gmail.com>
2026-05-28 23:23:04 +0700

    feat: implementasi fitur katalog skripsi berbasis Laravel MVC

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\KatalogBukuController;
+use App\Http\Controllers\KatalogSkripsiController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/buku', [KatalogBukuController::class, 'index']);
+Route::get('/skripsi', [KatalogSkripsiController::class, 'index']);
