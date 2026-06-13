3af13817593f5b6fc679d2b40e6db0b6d3af5af7
CatBounceXD <steventrue737@gmail.com>
2026-06-13 21:55:22 +0700

    chore: temp data fix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+});
+
 // Perkuliahan
 use App\Http\Controllers\Perkuliahan\BahanAjarController;
 use App\Http\Controllers\Perkuliahan\RpsController;
